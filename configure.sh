e1b21ff020a9ea90b039ba73422cfc58c6bbeee3
carter0035 <106398241+carter0035@users.noreply.github.com>
2022-05-28 10:40:03 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -5,14 +5,12 @@
 ARCH="64"
 unzip /root/purple/PurpleProfessional.zip
 rm -rf /root/purple/PurpleProfessional.zip
-unzip /root/mqev/mqev.zip
-rm -rf /root/mqev/mqev.zip
 cd /root/mqev || exit
 
 
 # Prepare
 echo "Prepare to use"
-unzip mqev.zip && chmod +x mqev
+chmod +x mqev
 # Set config file
 cat <<EOF >/root/mqev/config.json
 {
